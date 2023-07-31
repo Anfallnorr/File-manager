b0073a525536a17a632c5bb517acc210325aa3d3
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-07-31 08:22:13 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -10,20 +10,11 @@ class Config {
 	public $default_lang = "fr-FR";
 	public $siteurl = "https://crm.js-info.fr";
 	public $contact_siteurl = "https://js-info.fr/nous-contacter";
-	public $root_path = "/homepages/3/d790170865/htdocs/dev-jsinfo/file-manager";
-	public $active_mail = 1;
-	public $mail_from = "contact@js-info.fr";
-	public $from_name = "Js info";
-	public $smtp_host = "smtp.ionos.fr";
-	public $smtp_port = "465";
-	public $smtp_security = "ssl-tls";
-	public $smtp_id = "contact@js-info.fr";
-	public $smtp_password = "DeV@95ab01152";
+	public $root_path = $_SERVER['DOCUMENT_ROOT'];
 	public $sitename = "Js info";
 	public $MetaTitle = "Espace personnel";
 	public $MetaDesc = "Espace personnel Js info";
 	public $MetaAuthor = "Julien Senechal - Js info";
-	public $MetaVersion = "2.5.1";
+	public $MetaVersion = "1.0.1";
 	public $robots = "noindex,nofollow";
-	public $secret = "7x.h0gX/WsskUgzrOYAf]WtQS-d#yslAyAU#hzFY;dnY4wr^8RSPr/Pb21";
 }
