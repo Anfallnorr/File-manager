4f5fb96845bbf6a3befbe55b36734f4bd970adf9
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-07-31 09:02:38 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,18 +3,16 @@
 defined('_EXEC') or die;
 
 class Config {
-	public $theme = "vivo";
-	public $offline_message = "Ce site est en maintenance.<br /> Merci de revenir ultérieurement.";
 	public $debug = 0;
 	public $error_reporting = "devloppment";
 	public $default_lang = "fr-FR";
-	public $siteurl = "https://crm.js-info.fr";
+	public $siteurl = "https://file.js-info.fr";
 	public $contact_siteurl = "https://js-info.fr/nous-contacter";
 	public $root_path = $_SERVER['DOCUMENT_ROOT'];
 	public $sitename = "Js info";
 	public $MetaTitle = "Espace personnel";
 	public $MetaDesc = "Espace personnel Js info";
 	public $MetaAuthor = "Julien Senechal - Js info";
-	public $MetaVersion = "1.0.1";
+	public $MetaVersion = "1.0.2";
 	public $robots = "noindex,nofollow";
 }
