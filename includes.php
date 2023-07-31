84cddffec8445a36bed782205dfce98cd80388ae
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-07-31 08:11:39 +0200

    Delete includes.php

--- includes.php
+++
@@ -1,8 +0,0 @@
-<?php
-// require _ROOTURL_ ."/controllers/requests.php";
-require _ROOTURL_ ."/controllers/controllers.php";
-// require _ROOTURL_ ."/controllers/data.php";
-// require _ROOTURL_ ."/classes/filesystems.php";
-require _ROOTURL_ ."/classes/archiver.php";
-require _ROOTURL_ ."/assets/functions.php";
-?>
