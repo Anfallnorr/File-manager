013286715c9ecac492d0e4c41688dcc834d95def
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-01 09:03:26 +0200

    Delete templates/tpls directory

--- templates/tpls/breadcrumb.php
+++
@@ -1,7 +0,0 @@
-<ol class="breadcrumb">
-	<li class="breadcrumb-item">
-		<i class="bi bi-house"></i>
-		<a href="/" title="Accueil">Accueil</a>
-	</li>
-	<li class="breadcrumb-item breadcrumb-active">Mon drive</li>
-</ol>
--- templates/tpls/footer.php
+++
@@ -1,4 +0,0 @@
-
-<div class="app-footer">
-	<span>COPYRIGHT <?php echo date("Y") ?>. ALL_RIGHTS_RESERVED | <a href="/legals/legalnotice" title="LEGAL_NOTICE">LEGAL_NOTICE</a> | <a href="/legals/termsofuse" title="TERMS_OF_USE">TERMS_OF_USE</a></span>
-</div>
