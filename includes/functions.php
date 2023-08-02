8214b6a30a382b92580c823cfc2f1dc760ea0788
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-02 09:54:41 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,5 @@
 <?php
 
-// use Jsinfo\Langs;
-
 Global $exectutionTimeStart;
 $exectutionTimeStart = microtime(true);
 
@@ -48,7 +46,6 @@ function dump($debug): void {
 /* ************************************************************* */
 /* *********************** REQUIRE MULTI *********************** */
 /* ************************************************************* */
-// require_multi(_ROOTURL_ .'/config/configcontrollers.php', _ROOTURL_ .'/config/configviews.php', _ROOTURL_ .'/config/configfunctions.php');
 function require_multi(string $files): void {
     $files = func_get_args();
     foreach($files as $file) {
@@ -59,10 +56,6 @@ function require_multi(string $files): void {
 /* ********************************************************* */
 /* *********************** IN ARRAYS *********************** */
 /* ********************************************************* */
-// in_arrays($this->requests->view, $page_views)
-// in_arrays(
-	// in_arrays($this->requests->view, $page_views), $this->requests->seos, "id", "id_page_view"
-// )
 function in_arrays(array|string|int|null $needle, array $haystack, int|string $indexNeedle = "", int|string $indexHaystack = ""): ?array {
 	if( !is_null($needle) ) {
 		if( !empty($indexNeedle) && !empty($indexHaystack) ) {
@@ -85,15 +78,11 @@ function in_arrays(array|string|int|null $needle, array $haystack, int|string $i
 /* ************************************************************* */
 /* *********************** ARRAYS UNIQUE *********************** */
 /* ************************************************************* */
-// arrays_unique(
-	// in_arrays($this->requests->view, $page_views), $this->requests->seos, "id", "id_page_view"
-// )
 function arrays_unique(array $needle, string $byKey = "", bool $keepKey = false): ?array {
 	if( !is_null($needle) ) {
 		if (empty($byKey)) {
 			$uniqueArray = array_map("unserialize", array_unique(array_map("serialize", $needle)));
 		} else {
-			// Utilisation de array_reduce() pour dédoublonner le tableau
 			$uniqueData = array_reduce($needle, function ($carry, $item) use ($byKey) {
 				$key = strval($item[$byKey]);
 				
@@ -105,7 +94,6 @@ function arrays_unique(array $needle, string $byKey = "", bool $keepKey = false)
 			}, array());
 		}
 		
-		// Réindexation des clés du tableau résultant si $keepKey = false
 		$uniqueData = (!$keepKey) ? array_values($uniqueData) : $uniqueData;
 		
 		return $uniqueData ?? null;
@@ -139,13 +127,8 @@ function arrays_count_values(array $needle, string $byKey = "", string $bySubKey
 /* ************************ ARRAYS MULTI SORT ************************ */
 /* ******************************************************************* */
 function arrays_multisort(array $needle, string $column): array {
-	// Obtenez un tableau des "country_name" pour trier
 	$names = array_column($needle, $column);
-	
-	// Tri du tableau principal en utilisant les "$column"
 	array_multisort($names, $needle);
-	
-	// Tableau final trié par "country_name" avec les données correspondantes
 	$sorted = array();
 	
 	foreach ($needle as $value) {
@@ -154,33 +137,22 @@ function arrays_multisort(array $needle, string $column): array {
 		$sorted[$name][] = $value;
 	}
 	
-	// Affichage des résultats
 	return $sorted;
 }
 
 /* ******************************************************************** */
 /* ************************* GET EXPLODE FILE ************************* */
 /* ******************************************************************** */
-// getExplodeFile(_ROOTURL_ ."/var/tmp/navs.log", "|")
 function getExplodeFile(string $file, string $separator = "|"): ?array {
-	// Ouvrez le fichier en mode lecture
 	$handle = fopen($file, "r");
-	
-	// Tableau pour stocker les lignes du fichier
 	$results = array();
 	
-	// Parcourez le fichier ligne par ligne
 	while (($line = fgets($handle)) !== false) {
-		// Utilisez explode() pour séparer chaque ligne en tableau
 		$lineArray = explode($separator, $line);
-		
-		// Ajoutez le tableau de la ligne à votre tableau de lignes
 		$results[] = $lineArray;
 	}
 	
-	// Fermez le fichier
 	fclose($handle);
-	
 	return $results;
 }
 
@@ -251,33 +223,25 @@ function langDate(string $needle, string $isoLang = "fr"): string {
 /* *********************** GET IP *********************** */
 /* ****************************************************** */
 function getIp() {
-	// récupère l'adresse IP de l'utilisateur
 	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-		// IP si internet partagé
 		$ip = $_SERVER['HTTP_CLIENT_IP'];
 	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-		// IP derrière un proxy
 		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} else {
-		// Sinon : IP normale
 		$ip = $_SERVER['REMOTE_ADDR'];
 	}
 	
-	// si l'adresse IP est une adresse IPv6, on récupère l'adresse IPv4
 	if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
 		$ipv4 = '';
 		$ipv6_array = explode(':', $ip);
 		
-		// on parcourt les parties de l'adresse IPv6 à la recherche de l'adresse IPv4
 		foreach ($ipv6_array as $part) {
-			// si on a trouvé l'adresse IPv4, on l'enregistre et on sort de la boucle
 			if (filter_var($part, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 				$ipv4 = $part;
 				break;
 			}
 		}
 		
-		// si on a trouvé l'adresse IPv4, on l'utilise, sinon on utilise l'adresse IPv6
 		return $ipv4 ?: $ip;
 	} else {
 		return $ip;
@@ -288,7 +252,6 @@ function getGeoIp(bool $addLocation = true, bool $addNearby = true): ?array {
 	$results = array();
 	$ip = getIp();
 	$geoplugin = new geoPlugin();
-	// Si nous voulions changer la devise de base, nous décommenterions la ligne suivante
 	$geoplugin->locate($ip);
 	
 	if (!is_null($geoplugin->ip)) {
@@ -321,11 +284,9 @@ function getGeoIp(bool $addLocation = true, bool $addNearby = true): ?array {
 		}
 		
 		if ($geoplugin->currency != $geoplugin->currencyCode) {
-			// Notre visiteur n'utilise pas la même devise que la devise de base
 			$results['convert'] = "Au taux d'aujourd'hui, 100 $ US vous coûteront " . $geoplugin->convert(100);
 		}
 		
-		// Trouver des endroits à proximité
 		$nearby = $geoplugin->nearby();
 		
 		if ($addNearby == true && isset($nearby[0]['geoplugin_place'])) {
@@ -336,7 +297,6 @@ function getGeoIp(bool $addLocation = true, bool $addNearby = true): ?array {
 					'place' => $array['geoplugin_place'],
 					'country_code' => $array['geoplugin_countryCode'],
 					'region' => $array['geoplugin_region'],
-					// 'county' => $array['geoplugin_county'],
 					'latitude' => $array['geoplugin_latitude'],
 					'longitude' => $array['geoplugin_longitude'],
 					'distance_miles' => $array['geoplugin_distanceMiles'],
@@ -349,40 +309,6 @@ function getGeoIp(bool $addLocation = true, bool $addNearby = true): ?array {
 	} else {
 		return null;
 	}
-	
-}
-
-/* ************************************************************** */
-/* **************** CALCUL TTC / HT / TVA / PROMO *************** */
-/* ************************************************************** */
-// Avec la valeur décimale de TVA = taux de TVA/100
-function getHt(int|float $tax, int|float $ttc): int|float {
-	// PV HT =  Px TTC / (1+valeur décimale de TVA)
-	$getTax = $tax / 100;
-	$getPriceHt = $ttc / (1 + $getTax);
-	
-	return number_format($getPriceHt, 2, '.', '');
-}
-// $product['price'] - (($product['price'] * $product['promo']) / 100)
-function getTtc(int|float $tax, int|float $ht): int|float {
-	// PV TTC = Px HT * (1+ valeur décimale de TVA)
-	$getTax = $tax / 100;
-	$getPriceTtc = $ht * (1 + $getTax);
-	
-	return number_format($getPriceTtc, 2, '.', '');
-}
-
-function getTva(int|float $tax, int|float $ht): int|float {
-	// TVA = PV HT * valeur décimale de TVA
-	$getTax = $tax / 100;
-	$getPriceTva = $ht * $getTax;
-	
-	return number_format($getPriceTva, 2, '.', '');
-}
-
-function getPromo(int|float $price, int|float $promo): int|float {
-	// REMISE = Price - ((Price * Promo) / 100)
-	return $price - (($price * $promo) / 100);
 }
 
 /* *************************************************************** */
@@ -396,12 +322,9 @@ function getPersonnalFolder(int $id): string {
 /* ******************* REMOVE BACKSPACE ****************** */
 /* ******************************************************* */
 function removeBackspace(string $text, string $symbol, bool $pregReplace = false): string {
-	// $text = "Hello World !";
 	if( $pregReplace == true ) {
-		// $newText = preg_replace('/[^A-Za-z0-9 ]/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $text)); //supprime les caractères spéciaux et accentués
 		$newText = preg_replace('/[^\p{L}\p{N} ]/u', '', iconv('UTF-8', 'ASCII//TRANSLIT', $text)); // supprime les caractères spéciaux et accentués
 		
-		// Supprimer les symboles spécifiés
 		$symbolsToRemove = array(',', '\'', '"', ';', ':', '-', '_', '.', '(', ')', '[', ']', '+', '=');
 		foreach ($symbolsToRemove as $symbolToRemove) {
 			$newText = str_replace($symbolToRemove, '', $newText);
@@ -412,12 +335,10 @@ function removeBackspace(string $text, string $symbol, bool $pregReplace = false
 	
 	$newText = str_replace(' ', $symbol, $newText);
 	
-	// Supprimer le symbole en début de chaîne s'il y a
 	if (strpos($newText, $symbol) === 0) {
 		$newText = substr($newText, 1);
 	}
 	
-	// Supprimer le symbole en fin de chaîne s'il y a
 	if (substr($newText, -1) == $symbol) {
 		$newText = substr($newText, 0, -1);
 	}
