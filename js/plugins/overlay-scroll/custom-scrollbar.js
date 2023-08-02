0f661d770a89c39e71ac04d2a59377f298c5047b
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-02 10:02:30 +0200

    Add files via upload

---
+++ js/plugins/overlay-scroll/custom-scrollbar.js
@@ -0,0 +1,160 @@
+// var { 
+	// OverlayScrollbars, 
+	// ScrollbarsHidingPlugin, 
+	// SizeObserverPlugin, 
+	// ClickScrollPlugin  
+// } = OverlayScrollbarsGlobal;
+
+$(function() {
+	$(".sidebarMenuScroll").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".sidebarMenuScroll"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+$(function() {
+	$(".content-wrapper-scroll").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".content-wrapper-scroll"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+// Scroll 240
+$(function() {
+	$(".scroll240").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll240"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+// Scroll 500
+$(function() {
+	$(".scroll500").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll240"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+// Scroll 600
+$(function() {
+	$(".scroll600").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll240"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+// Scroll 370
+$(function() {
+	$(".scroll370").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll370"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+
+
+// Scroll 300
+$(function() {
+	$(".scroll300").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll300"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+
+
+
+// Scroll 206
+$(function() {
+	$(".scroll206").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll206"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+// Scroll 250
+$(function() {
+	$(".scroll250").overlayScrollbars({
+	// OverlayScrollbars(document.querySelector(".scroll250"), {
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
