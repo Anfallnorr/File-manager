358d5c23912bcfc87ba54860e0a6808bb9d5bbe0
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-03 09:40:09 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,11 +3,13 @@
 defined('_EXEC') or die;
 
 class Config {
-	// public $theme = "js-info";
-	public $theme = "vivo";
+	public $theme = "js-info";
+	// public $theme = "vivo";
 	public $debug = 0;
 	public $error_reporting = "devloppment";
-	public $default_lang = "fr-FR";
+	// public $default_lang = "fr-FR";
+	public $default_lang = "en-EN";
+	// public $default_lang = "de-DE";
 	public $siteurl = "https://crm.js-info.fr";
 	public $contact_siteurl = "https://js-info.fr/nous-contacter";
 	public $root_path = $_SERVER['DOCUMENT_ROOT'];
