25e4b36031a1fd067ffdf80929a168ff665363c0
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-03 10:44:35 +0200

    Update data.js

--- templates/js-info/html/datas/assets/js/data.js
+++ templates/js-info/html/datas/assets/js/data.js
@@ -187,7 +187,7 @@ addEventListener('mousedown', function handleMouseDown(event) {
 	let enableDragSelector = null;
 	let arrTarget = Array("BUTTON", "A", "INPUT");
 	
-	if (arrTarget.includes(eTarget.tagName) || eTarget.closest('.storage') || eTarget.closest('.shared-files-table') || eTarget.closest('.modal') || eTarget.closest('#dropzone_form')) {
+	if (arrTarget.includes(eTarget.tagName) || eTarget.closest('.storage') || eTarget.closest('.modal') || eTarget.closest('#dropzone_form')) {
 		return;
 	} else if (!eTarget.closest('.draggable')) {
 		enableDragSelector = dragSelector(event);
