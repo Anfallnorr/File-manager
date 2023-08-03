f158c236ff0cb0fceb6057bc1b90efa667f481a2
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-03 09:38:19 +0200

    Update topbar.php

--- templates/js-info/tpls/topbar.php
+++ templates/js-info/tpls/topbar.php
@@ -13,17 +13,17 @@
 			</div>
 			<div class="user-box dropdown">
 				<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" title="<?php echo $customer['firstname'] .' '. $customer['name'] ?>">
-					<img src="/templates/systems/img/default-avatar-hf.png" id="user_avatar" class="user-img" alt="user default avatar">
+					<img src="/templates/systems/img/default-avatar-hf.png" id="user_avatar" class="user-img" alt="<?php $langs->lang("USER_DEFAULT_AVATAR", "topbar") ?>">
 					<div class="user-info ps-3">
 						<p class="user-name mb-0"><?php echo $customer['firstname'] ?></p>
 						<p class="designattion mb-0"><?php echo $customer['name'] ?></p>
 					</div>
 				</a>
 				<ul class="dropdown-menu dropdown-menu-end">
-					<li><a class="dropdown-item" href="https://crm.js-info.fr" target="_blank" title="Créer un compte"><i class="bx bx-user"></i><span>Créer un compte</span></a></li>
+					<li><a class="dropdown-item" href="https://crm.js-info.fr" target="_blank" title="<?php $langs->lang("CREATE_AN_ACCOUNT", "topbar") ?>"><i class="bx bx-user"></i><span><?php $langs->lang("CREATE_AN_ACCOUNT", "topbar") ?></span></a></li>
 					<li><div class="dropdown-divider mb-0"></div></li>
-					<li><a class="dropdown-item" href="https://js-info.fr" target="_blank" title="Visiter le site Js info"><i class="bx bx-download"></i><span>Visiter le site Js info</span></a></li></li>
-					<li><a class="dropdown-item" href="https://js-info.fr/nous-contacter" target="_blank" title="Nous contacter"><i class="bx bx-lock"></i><span>Nous contacter</span></a></li></li>
+					<li><a class="dropdown-item" href="https://js-info.fr" target="_blank" title="<?php $langs->lang("VISIT_THE_JS_INFO_WEBSITE", "topbar") ?>"><i class="bx bx-download"></i><span><?php $langs->lang("VISIT_THE_JS_INFO_WEBSITE", "topbar") ?></span></a></li></li>
+					<li><a class="dropdown-item" href="https://js-info.fr/nous-contacter" target="_blank" title="<?php $langs->lang("CONTACT_US", "topbar") ?>"><i class="bx bx-lock"></i><span><?php $langs->lang("CONTACT_US", "topbar") ?></span></a></li></li>
 				</ul>
 			</div>
 		</nav>
