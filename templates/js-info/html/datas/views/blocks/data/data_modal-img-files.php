acaa06038d594cac72ff48d5752271d867f55ebe
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-03 09:11:36 +0200

    Update data_modal-img-files.php

--- templates/js-info/html/datas/views/blocks/data/data_modal-img-files.php
+++ templates/js-info/html/datas/views/blocks/data/data_modal-img-files.php
@@ -1,4 +1,3 @@
-<!-- BEGIN MODAL IMG FILES -->
 <div class="modal fade" id="modal_img_files" tabindex="-1" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-scrollable modal-lg">
 		<div class="modal-content">
@@ -17,4 +16,3 @@
 		</div>
 	</div>
 </div>
-<!-- END MODAL IMG FILES -->
