d7422aa1c77982aebbc3780d9a62579e20c05846
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-03 09:18:57 +0200

    Update data_modal-doc-files.php

--- templates/vivo/html/datas/views/blocks/data/data_modal-doc-files.php
+++ templates/vivo/html/datas/views/blocks/data/data_modal-doc-files.php
@@ -1,10 +1,9 @@
-<!-- BEGIN MODAL DOC FILES -->
 <div class="modal fade" id="modal_doc_files" tabindex="-1" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-scrollable modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Documents</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer" title="Fermer"></button>
+				<h5 class="modal-title"><?php $langs->lang("DATA_DOCUMENTS", "datas") ?></h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php $langs->lang("JSCLOSE") ?>" title="<?php $langs->lang("JSCLOSE") ?>"></button>
 			</div>
 			<div class="modal-body">
 				<?php foreach($docBasename as $key => $doc) {
@@ -12,9 +11,8 @@
 				} ?>
 			</div>
 			<div class="modal-footer">
-				<button type="button" class="btn btn-light" data-bs-dismiss="modal" title="Fermer"><i class="bi bi-arrow-left-circle"></i> Fermer</button>
+				<button type="button" class="btn btn-light" data-bs-dismiss="modal" title="<?php $langs->lang("JSCLOSE") ?>"><i class="bi bi-arrow-left-circle"></i> <?php $langs->lang("JSCLOSE") ?></button>
 			</div>
 		</div>
 	</div>
 </div>
-<!-- END MODAL DOC FILES -->
