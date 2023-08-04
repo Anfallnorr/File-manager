2be6e960f21af74c6a4650bcd80a85f42c36081d
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-04 13:33:23 +0200

    Update script.php

--- templates/vivo/tpls/script.php
+++ templates/vivo/tpls/script.php
@@ -3,17 +3,17 @@
 <script src="/templates/<?php echo $theme ?>/assets/js/bootstrap.bundle.min.js"></script>
 <script src="/js/functions.js" type="text/javascript"></script>
 
-<script src="/js/plugins/overlay-scroll/jquery.overlayScrollbars.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/overlay-scroll/custom-scrollbar.js" type="text/javascript" async></script>
+<script src="/js/plugins/overlay-scroll/jquery.overlayScrollbars.min.js" type="text/javascript"></script>
+<script src="/js/plugins/overlay-scroll/custom-scrollbar.js" type="text/javascript"></script>
 <script src="/js/plugins/datatables/dataTables.min.js" type="text/javascript"></script>
 <script src="/js/plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript"></script>
 <script src="/js/plugins/dropzone/dropzone.min.js" type="text/javascript"></script>
-<script src="/js/plugins/notifications/js/lobibox.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/notifications/js/notifications.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/notifications/js/notification-custom-script.js" type="text/javascript" async></script>
+<script src="/js/plugins/notifications/js/lobibox.min.js" type="text/javascript"></script>
+<script src="/js/plugins/notifications/js/notifications.min.js" type="text/javascript"></script>
+<script src="/js/plugins/notifications/js/notification-custom-script.js" type="text/javascript"></script>
 
-<script src="/templates/<?php echo $theme ?>/assets/js/main.min.js" type="text/javascript" async></script>
-<script src="/js/context-menu.js" type="text/javascript" async></script>
-<script src="/templates/<?php echo $theme ?>/assets/js/vivo.js" type="text/javascript" async></script>
-<script src="/templates/<?php echo $theme ?>/html/datas/assets/js/ajax-dropzone.js" type="text/javascript" async></script>
-<script src="/templates/<?php echo $theme ?>/html/datas/assets/js/data.js" type="text/javascript" async></script>
+<script src="/templates/<?php echo $theme ?>/assets/js/main.min.js" type="text/javascript"></script>
+<script src="/js/context-menu.js" type="text/javascript"></script>
+<script src="/templates/<?php echo $theme ?>/assets/js/vivo.js" type="text/javascript"></script>
+<script src="/templates/<?php echo $theme ?>/html/datas/assets/js/ajax-dropzone.js" type="text/javascript"></script>
+<script src="/templates/<?php echo $theme ?>/html/datas/assets/js/data.js" type="text/javascript"></script>
