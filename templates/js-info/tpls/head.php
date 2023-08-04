68c4e8ffa60337cf7c17dd669b7a1c924fa87527
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-04 13:34:08 +0200

    Update head.php

--- templates/js-info/tpls/head.php
+++ templates/js-info/tpls/head.php
@@ -12,16 +12,16 @@
 <link href="<?php echo $params['favicon_site'] ?>" type="image/vnd.microsoft.icon" rel="icon" />
 <link href="<?php echo $params['favicon_site'] ?>" type="image/x-icon" rel="shortcut icon" />
 
-<link href="/js/plugins/notifications/css/lobibox.min.css" type="text/css" rel="stylesheet" async />
-<link href="/js/plugins/simplebar/css/simplebar.min.css" type="text/css" rel="stylesheet" async />
-<link href="/js/plugins/perfect-scrollbar/css/perfect-scrollbar.min.css" type="text/css" rel="stylesheet" async />
-<link href="/js/plugins/metismenu/css/metisMenu.min.css" type="text/css" rel="stylesheet" async />
-<link href="/js/plugins/datatable/css/dataTables.bootstrap5.min.css" type="text/css" rel="stylesheet" async />
-<link href="/templates/<?php echo $this->theme ?>/assets/css/pace.minimal.css" type="text/css" rel="stylesheet" async />
-<script src="/templates/<?php echo $this->theme ?>/assets/js/pace.min.js" type="text/javascript" async></script>
-<link href="/js/plugins/dropzone/dropzone.min.css" rel="stylesheet" async />
+<link href="/js/plugins/notifications/css/lobibox.min.css" type="text/css" rel="stylesheet" />
+<link href="/js/plugins/simplebar/css/simplebar.min.css" type="text/css" rel="stylesheet" />
+<link href="/js/plugins/perfect-scrollbar/css/perfect-scrollbar.min.css" type="text/css" rel="stylesheet" />
+<link href="/js/plugins/metismenu/css/metisMenu.min.css" type="text/css" rel="stylesheet" />
+<link href="/js/plugins/datatable/css/dataTables.bootstrap5.min.css" type="text/css" rel="stylesheet" />
+<link href="/templates/<?php echo $this->theme ?>/assets/css/pace.minimal.css" type="text/css" rel="stylesheet" />
+<script src="/templates/<?php echo $this->theme ?>/assets/js/pace.min.js" type="text/javascript"></script>
+<link href="/js/plugins/dropzone/dropzone.min.css" rel="stylesheet" />
 
-<link href="/templates/<?php echo $this->theme ?>/assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" async />
-<link href="/templates/<?php echo $this->theme ?>/assets/css/app.min.css" type="text/css" rel="stylesheet" async />
-<link href="/templates/<?php echo $this->theme ?>/assets/css/icons.min.css" type="text/css" rel="stylesheet" async />
-<link href="/templates/<?php echo $this->theme ?>/assets/css/js-info.css" type="text/css" rel="stylesheet" async />
+<link href="/templates/<?php echo $this->theme ?>/assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+<link href="/templates/<?php echo $this->theme ?>/assets/css/app.min.css" type="text/css" rel="stylesheet" />
+<link href="/templates/<?php echo $this->theme ?>/assets/css/icons.min.css" type="text/css" rel="stylesheet" />
+<link href="/templates/<?php echo $this->theme ?>/assets/css/js-info.css" type="text/css" rel="stylesheet" />
