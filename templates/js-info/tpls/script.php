b54f05f017df21b97e2629619fd299b5b1a258de
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-04 13:31:43 +0200

    Update script.php

--- templates/js-info/tpls/script.php
+++ templates/js-info/tpls/script.php
@@ -1,14 +1,14 @@
 <?php defined('_EXEC') or die; ?>
 <script src="/js/plugins/jquery/jquery-3.7.0.js" type="text/javascript"></script>
 <script src="/templates/<?php echo $this->theme ?>/assets/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-<script src="/js/functions.js" type="text/javascript" async></script>
+<script src="/js/functions.js" type="text/javascript"></script>
 
 <script src="/js/plugins/simplebar/js/simplebar.min.js" type="text/javascript" async></script>
 <script src="/js/plugins/metismenu/js/metisMenu.min.js" type="text/javascript" async></script>
 <script src="/js/plugins/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/datatables/dataTables.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript" async></script>
-<script src="/js/plugins/dropzone/dropzone.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/datatables/dataTables.min.js" type="text/javascript"></script>
+<script src="/js/plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<script src="/js/plugins/dropzone/dropzone.min.js" type="text/javascript"></script>
 <script src="/js/plugins/notifications/js/lobibox.min.js" type="text/javascript" async></script>
 
 <script src="/templates/<?php echo $this->theme ?>/assets/js/app.js" type="text/javascript" async></script>
