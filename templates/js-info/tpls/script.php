97804da1fab649b729620e0a4b28a246f7a6b5bd
Julien <90765278+Anfallnorr@users.noreply.github.com>
2023-08-04 12:29:32 +0200

    Update script.php

--- templates/js-info/tpls/script.php
+++ templates/js-info/tpls/script.php
@@ -1,18 +1,18 @@
 <?php defined('_EXEC') or die; ?>
 <script src="/js/plugins/jquery/jquery-3.7.0.js" type="text/javascript"></script>
-<script src="/templates/<?php echo $this->theme ?>/assets/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-<script src="/js/functions.js" type="text/javascript"></script>
-<!-- Plugins -->
-<script src="/js/plugins/simplebar/js/simplebar.min.js" type="text/javascript"></script>
-<script src="/js/plugins/metismenu/js/metisMenu.min.js" type="text/javascript"></script>
-<script src="/js/plugins/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript"></script>
-<script src="/js/plugins/datatables/dataTables.min.js" type="text/javascript"></script>
-<script src="/js/plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript"></script>
-<script src="/js/plugins/dropzone/dropzone.min.js" type="text/javascript"></script>
-<script src="/js/plugins/notifications/js/lobibox.min.js" type="text/javascript"></script>
-<!-- app JS -->
-<script src="/templates/<?php echo $this->theme ?>/assets/js/app.js" type="text/javascript"></script>
-<script src="/js/context-menu.js" type="text/javascript"></script>
-<script src="/templates/<?php echo $this->theme ?>/assets/js/js-info.js" type="text/javascript"></script>
-<script src="/templates/<?php echo $this->theme ?>/html/datas/assets/js/ajax-dropzone.js" type="text/javascript"></script>
-<script src="/templates/<?php echo $this->theme ?>/html/datas/assets/js/data.js" type="text/javascript"></script>
+<script src="/templates/<?php echo $this->theme ?>/assets/js/bootstrap.bundle.min.js" type="text/javascript" async></script>
+<script src="/js/functions.js" type="text/javascript" async></script>
+
+<script src="/js/plugins/simplebar/js/simplebar.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/metismenu/js/metisMenu.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/datatables/dataTables.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/datatables/dataTables.bootstrap.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/dropzone/dropzone.min.js" type="text/javascript" async></script>
+<script src="/js/plugins/notifications/js/lobibox.min.js" type="text/javascript" async></script>
+
+<script src="/templates/<?php echo $this->theme ?>/assets/js/app.js" type="text/javascript" async></script>
+<script src="/js/context-menu.js" type="text/javascript" async></script>
+<script src="/templates/<?php echo $this->theme ?>/assets/js/js-info.js" type="text/javascript" async></script>
+<script src="/templates/<?php echo $this->theme ?>/html/datas/assets/js/ajax-dropzone.js" type="text/javascript" async></script>
+<script src="/templates/<?php echo $this->theme ?>/html/datas/assets/js/data.js" type="text/javascript" async></script>
